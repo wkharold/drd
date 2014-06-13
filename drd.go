ed818bbd7fc1addbdd3f30e56b53a71df0a27de4
Ward K Harold <wkharold@gmail.com>
2014-06-12 22:56:03 -0500

    Add telemetry flag

--- drd.go
+++ drd.go
@@ -9,13 +9,14 @@ import (
 )
 
 var (
-	fladdr = flag.String("addr", ":3080", "registry address")
+	fladdr      = flag.String("addr", ":3080", "registry address")
+	fltelemetry = flag.Bool("telemetry", false, "provide service telemetry")
 )
 
 func main() {
 	flag.Parse()
 
-	dr, err := dr.New()
+	dr, err := dr.New(dr.Context{*fltelemetry})
 	if err != nil {
 		log.Fatal(err)
 	}
